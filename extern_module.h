11b7e65ed93c5c3a2b5a1dedb96132c3fd6b97c0
Michael Zhang <mz487@atlas.cs.rutgers.edu>
2021-04-06 19:50:11 -0400

    extern module stuff

--- extern_module.h
+++ extern_module.h
@@ -14,19 +14,19 @@ extern pthread_mutex_t dir_term_mutex;
 extern pthread_mutex_t file_term_mutex;
 
 /** track number of waiting directory threads and file threads */
-extern int no_waiting_dirs;
-extern int no_waiting_files;
+extern unsigned int no_waiting_dirs;
+extern unsigned int no_waiting_files;
 
-extern int no_dir_threads;
-extern int no_file_threads;
+extern unsigned int no_dir_threads;
+extern unsigned int no_file_threads;
 
 /** conditional variables for use in signalling and waiting the directory threads, and file threads */
 extern pthread_cond_t cond_dir;
 extern pthread_cond_t cond_file;
 
 /** flags that will let the threads determine whether to terminate or not */
-extern int dir_threads_terminate;
-extern int file_threads_terminate;
+extern unsigned int dir_threads_terminate;
+extern unsigned int file_threads_terminate;
 
 /** the directory and file queues that should be shared with all directory and file threads */
 extern sync_queue *directory_queue;
@@ -38,7 +38,7 @@ extern wf_repo *wf_stack;
 /** pointers to wf_tables to help in the jsd/analysis work assigning portion */
 extern wf_table *x;
 extern wf_table *y;
-extern int jsd_comp_iter;
-extern int jsd_total_comp;
+extern unsigned int jsd_comp_iter;
+extern unsigned int jsd_total_comp;
 extern jsd_entry **jsd_list;
 #endif
