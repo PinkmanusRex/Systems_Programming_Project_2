33dff82474bdf7fa389c71213e3deb8c9d5bbad5
Kev Sharma <skevkets@gmail.com>
2021-04-07 16:38:18 -0400

    tested helperR.c

--- helperR.h
+++ helperR.h
@@ -1,3 +1,21 @@
+/* Used for generateFilePath */
+typedef struct {
+    size_t length;
+    size_t used;
+    char *data;
+} strbuf_t;
+
+int sb_initk(strbuf_t *, size_t);
+void sb_destroyk(strbuf_t *);
+int sb_appendk(strbuf_t *, char); // put char at used, ++used (double if nec)
+int sb_removek(strbuf_t *, char*); // removes last char -> used-1
+int sb_resetk(strbuf_t *); // used for project 1
+
+/* additional one */
+int sb_concatk(strbuf_t *, char*); // append cannot append strings
+
+
+/* Used for arraylists amortized concurrent opertaions within ddirectory routine */
 typedef struct node{
     char* value;
     struct node* next;
@@ -21,3 +39,5 @@ char* generateFilePath(char* directoryName, char* currPath);
     If fileName doesn't end with fileSuffix, returns 0
 */
 int endsWithSuffix(char* fileSuffix, char* fileName);
+
+
