9a8c7bb55d14fd7e0578393030f430fb89d9e2eb
Kev Sharma <skevkets@gmail.com>
2021-04-07 20:49:47 -0400

    TODO tasks + thread typo fix

--- collection_threads.h
+++ collection_threads.h
@@ -1,6 +1,6 @@
 #ifndef COLLECTION_THREADS_H
 #define COLLECTION_THREADS_H
-void *dir_threat_routine(void *arg);
+void *dir_thread_routine(void *arg);
 void *file_thread_routine(void *arg);
 
 
--- compare.c
+++ compare.c
@@ -14,7 +14,6 @@
 #include "stringbuf.h"
 #include "wf_table.h"
 #include "helperR.h"
-#include "strbuf.h"
 #include "collection_threads.h"
 #include "debugger.h"
 #include "sync_queue.h"
@@ -104,19 +103,122 @@ int main(int argc, char** argv){
     if(initializeOptions(argc, argv) == EXIT_FAILURE){
         free(suffix);
         perror("Invalid -option arguments, program halt & terminated");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
-    printf("dN = %u \n", dN);
-    printf("fN = %u \n", fN);
-    printf("aN = %u \n", aN);
-    printf("suffix = %s + %ld\n", suffix, strlen(suffix));
-    free(suffix);
+    //printf("dN = %u \n", dN);
+    //printf("fN = %u \n", fN);
+    //printf("aN = %u \n", aN);
+    //printf("suffix = %s + %ld\n", suffix, strlen(suffix));
+
     /**
      * TODO:
      *  initialize the queues and the relevant extern variables
      *  fill the queues with the preliminary directories and files as taken from the command-line
      */
+
+    // Initialize and populate the synchronized queues
+    directory_queue = sync_q_create();
+    file_queue = sync_q_create();
+
+    if(directory_queue == NULL || file_queue == NULL){
+        perror("Failure to initialize a syncronized queue (file and/or directory)");
+        exit(EXIT_FAILURE);
+    }
+
+    // Siphon off arguments from argv into respective queue.
+    struct stat argumentData;
+    int siphonedToFileQueue;
+    int siphonedToDirQueue;
+    for(int i=1; i<argc; ++i){
+        /* I allow argv arguments to start with a .
+            For example: ./compare ../ is a valid argument*/
+
+        if(argv[i][0] == '-'){
+            continue; // options have already been initialized.
+        }
+        if(stat(argv[i], &argumentData) == -1){
+            // argv[i] doesn't exist 
+            perror(argv[i]);
+            continue;
+        }
+
+        // Now that we know argv[i] has a valid address and isn't an option flag
+        // We can move onto siphoning to respective queue provided it has read permissions.
+        char* pathname = (char *) malloc(sizeof(char) * strlen(argv[i]) + 2);
+        strcpy(pathname, argv[i]); // Needed since directoryFunction_r frees.
+        
+        if(S_ISREG(argumentData.st_mode)){
+            // If we can't open the regular file, continue forward.
+            int inputFD = open(pathname, O_RDONLY);
+            if(inputFD == -1){
+                perror(pathname);
+                free(pathname);  // important because we aren't exiting.
+                continue;
+            }
+            close(inputFD);
+            
+            // Add to file Queue
+            siphonedToFileQueue = sync_q_add(file_queue, pathname);
+            if(siphonedToFileQueue == EXIT_FAILURE){
+                perror("sync_q library line 62");
+                free(pathname);
+                continue;
+            }
+        }
+        else if(S_ISDIR(argumentData.st_mode)){
+            // If the directory cannot be opened, no point in adding it
+            DIR* temporaryCheckForPerms = opendir(pathname);
+            if(temporaryCheckForPerms == NULL){
+                perror(pathname);
+                free(pathname);
+                continue;
+            }
+            closedir(temporaryCheckForPerms);
+
+            // Add to directoryQueue
+            siphonedToDirQueue = sync_q_add(directory_queue, pathname);
+            if(siphonedToDirQueue == EXIT_FAILURE){
+                perror("sync_q library line 62");
+                free(pathname);
+                continue;
+            }
+        }
+        else{
+            free(pathname);
+        }
+    }
+
+        // There is no point in moving on if nothing further to do.
+    if(sync_q_empty(file_queue) && sync_q_empty(directory_queue)){
+        perror("main thread did not populate queues. Terminating");
+        sync_q_destroy(directory_queue); sync_q_destroy(file_queue);
+        free(suffix);
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize relevant extern variables and flags
+    no_waiting_dirs = 0;
+    no_waiting_files = 0;
+    no_dir_threads = dN;
+    no_file_threads = fN;
+
+    dir_threads_terminate = 0;
+    file_threads_terminate = 0;
+    
+    // Initialize relevant extern mutexes and conditional vars
+    if(pthread_mutex_init(&dir_term_mutex, NULL) != 0)
+        {perror("mutex init failure"); exit(EXIT_FAILURE);};
+    
+    if(pthread_mutex_init(&file_term_mutex, NULL) != 0)
+        {perror("mutex init failure"); exit(EXIT_FAILURE);};
+
+    if(pthread_cond_init(&cond_dir, NULL) != 0)
+        {perror("cond init failure"); exit(EXIT_FAILURE);};
+    
+    if(pthread_cond_init(&cond_file, NULL) != 0)
+        {perror("cond init failure"); exit(EXIT_FAILURE);};
+
 
     /**
      * TODO:
@@ -125,6 +227,95 @@ int main(int argc, char** argv){
      *  join the directory threads and then the file threads
      *  cleanup the queues and all the shared resources that are not relevant or needed for the analysis phase
      */
+
+    // pthread_t is a variable used to track the Thread id of created thread
+    pthread_t* directoryTID = (pthread_t*) malloc(no_dir_threads * sizeof(pthread_t));
+    if(directoryTID == NULL){
+        perror("malloc failed to initialize array of directory pthreads");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t* filesTID = (pthread_t*) malloc(no_file_threads * sizeof(pthread_t));
+    if(filesTID == NULL){
+        perror("malloc failed to initialize array of file pthreads");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Create directory and file threads, and pass in their routines */
+    int create;
+    for(int i=0; i < no_dir_threads; i++){
+        // check to make sure that the last condition is true, our file_queue/dir_queue are global so I pass NULL
+        create = pthread_create(&directoryTID[i], NULL, dir_thread_routine, NULL); 
+        if(create != 0){
+            perror("Error creating directory thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for(int i=0; i < no_file_threads; i++){
+        // check to make sure that the last condition is true, our file_queue/dir_queue are global so I pass NULL
+        create = pthread_create(&filesTID[i], NULL, file_thread_routine, NULL); 
+        if(create != 0){
+            perror("Error creating file thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+        /* !! Wait for directory threads and file threads to do their job !! */
+    int join;
+    for(int i=0; i < no_dir_threads; i++){
+        join = pthread_join(directoryTID[i], NULL); // thread routines don't return anything.
+        if(join != 0){
+            perror("Error joining directory thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for(int i=0; i < no_file_threads; i++){
+        join = pthread_join(filesTID[i], NULL); // thread routines don't return anything.
+        if(join != 0){
+            perror("Error joining directory thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Now that they are done doing their work, we can free memory allocated for their jobs.
+    // But first ensure they are empty.
+    if(!(sync_q_empty(file_queue) && sync_q_empty(directory_queue))){
+        perror("Something went wrong in routines.");
+        free(suffix);
+        exit(EXIT_FAILURE);
+    }
+
+    // Deallocate all the things that aren't necessary for analysis phase.
+
+    sync_q_destroy(file_queue); // Don't have to check return value since it will always be success.
+    sync_q_destroy(directory_queue); // if it isnt' success, then it terminates and we don't get here anyway.
+
+    free(suffix); // Don't need this for analysis phase.
+
+    int mutex_status = 0;
+    mutex_status = pthread_mutex_destroy(&dir_term_mutex);
+    if (mutex_status != 0) {
+        perror("Mutex destroy error dirTermMutex");
+        exit(EXIT_FAILURE);
+    }
+    mutex_status = pthread_mutex_destroy(&file_term_mutex);
+    if (mutex_status != 0) {
+        perror("Mutex destroy error fileTermMutex");
+        exit(EXIT_FAILURE);
+    }
+
+    int cond_status = 0;
+    cond_status = pthread_cond_destroy(&cond_dir);
+    if(cond_status != 0){
+        perror("condition variable destroy error cond_dir");
+        exit(EXIT_FAILURE);
+    }
+
+    cond_status = pthread_cond_destroy(&cond_file);
+    if(cond_status != 0){
+        perror("condition variable destroy error cond_file");
+        exit(EXIT_FAILURE);
+    }
 
     /** the debug print of the contents of the wf_repo */
     if (DEBUG) {
